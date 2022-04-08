f2c553bf3fc11f3843c74f1d270db382f58d43f2
valentinfrlch <valentin.froehlich@gmail.com>
2022-04-08 15:55:12 +0100

    up

--- heatmap.py
+++ heatmap.py
@@ -53,7 +53,57 @@ def heatmap():
     #webbrowser.open("heatmap.html")
 
 
-heatmap()
-
-
 # show commute time as heatmap
+def commute_heatmap():
+    """create heatmap for commute time"""
+    # the longer the commute the more the heatmap will be red
+    # the shorter the commute the more the heatmap will be blue
+    # create a map
+    map = folium.Map(location=[47.31, 8.79], zoom_start=12)
+    # create a heatmap layer
+    # ignore NoneType
+    # get lat, long and commute if not None
+
+    # get max and min commute time
+    commute_times = []
+    for student in data:
+        if student["commute"] is not None:
+            commute_times.append(student["commute"])
+    max_commute = max(commute_times)
+    # get the color for every student
+    for student in data:
+        if student["lat"] is not None and student["long"] is not None and student["commute"] is not None:
+            color = dynamic_color(student["commute"], max_commute)
+            folium.CircleMarker(
+                [student["lat"], student["long"]],
+                radius=10,
+                popup=student["name"] + ' ' + student["vorname"],
+                color=color,
+                fill=True,
+                fill_color=color,
+                fill_opacity=1).add_to(map)
+    # save the map
+    map.save("commute_heatmap.html")
+    # open the map in the browser
+    webbrowser.open("commute_heatmap.html")
+
+
+def dynamic_color(commute, max_commute):
+    """
+    calculate color based on delta to max and min commute
+    the more the delta the more the color will be white
+    """
+    def color(percentage):
+        """multiply percentage by 255 and round to int"""
+        # convert to hex red
+        str(hex(int(percentage * 2.55)) + 'ff')[2:]
+        return int(percentage * 2.55)
+    # calculate percentage of max commute
+    percentage = (commute / max_commute) * 100
+    # calculate the color based on the percentage
+    # the closer to 100 the more the color will be red
+    # the closer to 0 the more the color will be white
+    return color(percentage)
+
+
+commute_heatmap()
