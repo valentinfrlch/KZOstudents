97ccb3a6b9dd058c74442a5a56ec52cc92ba542d
valentinfrlch <valentin.froehlich@gmail.com>
2022-04-09 11:57:35 +0200

    map update

--- heatmap.py
+++ heatmap.py
@@ -1,5 +1,6 @@
 # draw a heatmap for the commute time
 # read data from excel file
+from turtle import fillcolor
 import folium
 import shapely
 import scipy
@@ -49,22 +50,20 @@ def heatmap():
 
     heat_layer = HeatMap(data=d, blur=100, radius=50)
     # add the heatmap layer to the map
-    map.add_child(heat_layer)
-    # save the map
-    map.save("index.html")
-    # open the map in the browser
-    # webbrowser.open("heatmap.html")
+    return heat_layer
 
 
 # show commute time as heatmap
 def commute_heatmap():
 
+    APIKEY = "pk.eyJ1IjoidmFsZW50aW5mcmxjaCIsImEiOiJjazk4ZzQ3MHcwNmJqM3FybzJxMXh1d2U1In0.aGuJSQwT1ub9friABW4HpQ"
     color = "red"
 
     # connect every lat, long to (47.31783342759546, 8.795774929882974)
     # create a map
     map = folium.Map(location=[47.31783342759546,
-                     8.795774929882974], zoom_start=12)
+                     8.795774929882974], zoom_start=12, tiles='https://api.mapbox.com/styles/v1/valentinfrlch/cl1rnamlr000214t8kpkn0mev/tiles/256/{z}/{x}/{y}@2x?access_token=' + APIKEY, attr="mapbox", name="Map")
+
 
     # get all commute times
     durations = {}
@@ -82,33 +81,46 @@ def commute_heatmap():
                 (student["lat"], student["long"]))
 
     # for every key in durations draw a line between the values
+    circles = []
+    polygons = []
     for key in durations:
-        # if lenght is 1 draw a circle with distance as the radius and 47.31783342759546, 8.795774929882974 as the center
-        if len(durations[key]) == 1:
-            folium.Circle(location=(47.31783342759546, 8.795774929882974), radius=distance(
-                durations[key][0][0], durations[key][0][1], 47.31783342759546, 8.795774929882974), color=color).add_to(map)
-        elif len(durations[key]) == 2:
-            # use the average of the two lat, long as radius
-            radius = (distance(durations[key][0][0], durations[key][0][1], durations[key][1][0], durations[key][1][1]) + distance(
-                durations[key][0][0], durations[key][0][1], 47.31783342759546, 8.795774929882974)) / 2 * 1000
-            folium.Circle(
-                location=(47.31783342759546, 8.795774929882974), radius=radius, color=color).add_to(map)
-        else:
-            # calculate the convex hull of the points
-            hull = shapely.geometry.MultiPoint(durations[key]).convex_hull
-            x, y = hull.exterior.coords.xy
-            # convert to tuples
-            x = list(x)
-            y = list(y)
-            points = [(x[i], y[i])
-                      for i in range(0, len(x))]
-            # interpolate the points
+        if key in [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]:
+            # if lenght is 1 draw a circle with distance as the radius and 47.31783342759546, 8.795774929882974 as the center
+            if len(durations[key]) == 1:
+                circles.append([key, folium.Circle(location=(47.31783342759546, 8.795774929882974), radius=distance(
+                    durations[key][0][0], durations[key][0][1], 47.31783342759546, 8.795774929882974),
+                    color=color, opacity=0.1, fill=True, fillcolor="red", fill_opacity=0.1,
+                    tooltip="commute time: " + str(key) + "min").add_to(map, name="commute time: " + str(key) + "min")])
+            elif len(durations[key]) == 2:
+                # use the average of the two lat, long as radius
+                radius = (distance(durations[key][0][0], durations[key][0][1], durations[key][1][0], durations[key][1][1]) + distance(
+                    durations[key][0][0], durations[key][0][1], 47.31783342759546, 8.795774929882974)) / 2 * 1000
+                circles.append([key, folium.Circle(
+                    location=(47.31783342759546, 8.795774929882974), radius=radius, color=color,
+                    opacity=0.1, fill=True, fillcolor="red", fill_opacity=0.1, tooltip="commute time: " + str(key) + "min")])
+            else:
+                # calculate the convex hull of the points
+                hull = shapely.geometry.MultiPoint(durations[key]).convex_hull
+                x, y = hull.exterior.coords.xy
+                # convert to tuples
+                x = list(x)
+                y = list(y)
+                points = [(x[i], y[i])
+                          for i in range(0, len(x))]
+                # interpolate the points
 
-            # draw a polygon
-            folium.Polygon(
-                locations=points, color=color, tooltip=key, width=).add_to(map)
+                # draw a polygon
+                polygons.append([key, folium.Polygon(
+                    locations=points, color=color, tooltip="commute time: " + str(key) + "min", width=1, opacity=0.3, fill=True, fillcolor="red", fill_opacity=0.1)])
 
     # save the map
+    heatmap().add_to(map, name="heatmap")
+    #reverse for loop
+    for i in range(len(circles) - 1, -1, -1):
+        circles[i][1].add_to(map)
+    for i in range(len(polygons) - 1, -1, -1):
+        polygons[i][1].add_to(map)
+    folium.LayerControl(collapsed=False).add_to(map)
     map.save("index.html")
     # open the map in the browser
     webbrowser.open("index.html")
